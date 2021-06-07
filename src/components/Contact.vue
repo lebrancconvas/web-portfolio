14be1f4c228b0df05999c8674f48fd5cce56cbc0
Poom Yimyuean <lebrancconvas@gmail.com>
2021-06-08 01:24:35 +0700

    :sparkles: Add Contact List in Contact Section.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <h1>Contact</h1>
+        <a href="https://facebook.com/houseofscarletth">
+            <img src="@/assets/facebook-logo.png" type="image/png" alt="Facebook Logo"> 
+        </a>
+        <a href="https://discord.gg/N8aPf5hQBP">
+            <img src="@/assets/discord-logo.png" type="image/png" alt="Discord Logo">
+        </a>
+        <a href="https://github.com/lebrancconvas">
+            <img src="@/assets/github-logo.png" type="image/png" alt="Github Logo"> 
+        </a>
+        <a href="mailto:poomkunmega@gmail.com">
+            <img src="@/assets/gmail-logo.png" type="image/png" alt="Gmail Logo"> 
+        </a> 
     </div>
 </template>
 
@@ -11,5 +23,9 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+    width: 80px; 
+    height: 80px;
+    margin: 20px 20px 20px 20px;  
+}
 </style> 
